9370d133730fdc65482332fbe697b2b98feff1d5
= <2654189525@qq.com>
2018-08-15 13:58:54 +0800

    实现了错误需求

--- readxls.py
+++ readxls.py
@@ -11,20 +11,28 @@ def get_row_contents(acticity_id):
             return table.row_values(item)
 
 
-def handle_open_type_1(row_data):
+def handle_open_type_2(row_data, check_time):
+
     pass
 
 
-def handle_open_type_2(row_data):
-    print('*' * 80)
-    pass
+def handle_open_type_1(row_data, check_time):
+    start_time = row_data[4]
+    end_time = row_data[5]
+    tmp = int(check_time)
+    if tmp < int(end_time) and tmp > int(start_time):
+        return True
 
 
-def is_open(row_data):
+def is_open(row_data, check_time):
+    if row_data[6] == 1:
+        handle_open_type_1(row_data, check_time)
     if row_data[6] == 2:
-        handle_open_type_2(row_data)
+        handle_open_type_2(row_data, check_time)
+
+    return False
 
 
 if __name__ == '__main__':
-    row_data = get_row_contents(1)
-    is_open(row_data)
+    row_data = get_row_contents(32)
+    is_open(row_data, '20180703000000')
