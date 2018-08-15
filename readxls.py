0f1a0d24aa7a88f701ad35acb99959713704a7e2
= <2654189525@qq.com>
2018-08-15 15:48:53 +0800

    将日期的比较从数值转换为使用自带的日期比较函数进行比较,修复open_type为1 的bug:未考虑开服多少天后开启

--- readxls.py
+++ readxls.py
@@ -1,8 +1,13 @@
 import xlrd
 import config
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+
 sheets = xlrd.open_workbook('activity.xls')
 table = sheets.sheet_by_name('新服')
 rows = table.nrows
+
 
 def get_row_contents(acticity_id):
     for item in range(2, table.nrows):
@@ -11,16 +16,37 @@ def get_row_contents(acticity_id):
 
 
 def handle_open_type_2(row_data, check_time):
-
     pass
 
 
+def str2date(str_time, format='%Y%m%d%H%M%S'):
+    return datetime.strptime(str_time, format).date()
+
+
 def handle_open_type_1(row_data, check_time):
-    start_time = int(config.OPEN_TYPE_DEFAULT_VALUE)
-    during_time = row_data[7] * 1000000
-    tmp = int(check_time)
-    if tmp >= start_time and tmp <= during_time + start_time:
+    '''
+    实现方法:
+        1. 把所有有关时间的数据(字符串)都转换为datetime数据类型
+        1. 比较时间
+    :param row_data:
+    :param check_time:
+    :return:
+    '''
+    start_time = str2date(config.OPEN_TYPE_DEFAULT_VALUE)
+
+    if row_data[9] == '':
+        open_time = timedelta()
+    else:
+        open_time = timedelta(days=int(row_data[9]))
+
+    during_time = timedelta(days=row_data[7])
+    tmp = str2date(check_time)
+    down = start_time + open_time
+    up = during_time + open_time + start_time
+    # print('{},{},{},{}'.format(open_time, down, tmp, up))
+    if tmp >= down and tmp <= up:
         return True
+
 
 def is_open(row_data, check_time):
     if row_data[6] == 1:
@@ -32,10 +58,15 @@ def is_open(row_data, check_time):
 
 def test_2():
     row_data = get_row_contents(11)
-    print(is_open(row_data, '20180702000000'))
+    print(is_open(row_data, '20180716000000'))
+    print(is_open(row_data, '20180717000000'))
+    print(is_open(row_data, '20180718000000'))
+    print(is_open(row_data, '20180719000000'))
+    print(is_open(row_data, '20180720000000'))
 
-def test_1():
-    row_data = get_row_contents(11)
+
+def test1_1(row_data=get_row_contents(11)):
+    print('in test1_1'.center(40,'*'))
     print(is_open(row_data, '20180702000000'))
     print(is_open(row_data, '20180703000000'))
     print(is_open(row_data, '20180704000000'))
@@ -45,7 +76,22 @@ def test_1():
     print(is_open(row_data, '20180708000000'))
     print(is_open(row_data, '20180709000000'))
 
+def test1_2(row_data=get_row_contents(106)):
+    print('in test1_2'.center(40,'*'))
+    print(is_open(row_data, '20180716000000'))
+    print(is_open(row_data, '20180717000000'))
+    print(is_open(row_data, '20180718000000'))
+    print(is_open(row_data, '20180719000000'))
+    print(is_open(row_data, '20180720000000'))
+    print(is_open(row_data, '20180721000000'))
+    print(is_open(row_data, '20180722000000'))
+    print(is_open(row_data, '20180723000000'))
+    print(is_open(row_data, '20180724000000'))
+    print(is_open(row_data, '20180725000000'))
+
+
 
 if __name__ == '__main__':
-    # test_1()
-    test_2()
+    test1_1()
+    test1_2()
+    # test_2()
